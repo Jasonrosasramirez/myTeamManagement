0600c56149f24e88ceed9f520e1ab9794bb9c116
Jason <jrosasrjf@gmail.com>
2021-04-30 22:24:32 -0500

    used npm install console.table --save

--- inquirer.js
+++ inquirer.js
@@ -1,1 +1,27 @@
 const inquirer = require("inquirer");
+
+questionsArray = ["how is the weather today?"];
+
+// call once somewhere in the beginning of the app
+const cTable = require('console.table');
+console.table([
+  {
+    name: 'foo',
+    age: 10
+  }, {
+    name: 'bar',
+    age: 20
+  }
+]);
+
+
+
+// prints
+/*
+
+name  age
+----  ---
+foo   10
+bar   20
+
+*/
