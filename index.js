5055b93e359388e2fbb04db9ab50c8231e01f6ae
Jason <jrosasrjf@gmail.com>
2021-05-02 19:37:05 -0500

    created a connection variable as an object that will connect to mySQL

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 /*   Referemces to nmp Packages/ installations   */
+
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+
+/*  Link to my SQL Server  */
+
+const connection = mySql.createConnection({
+
+    host: "localhost",
+    port: process.env.PORT || 3000,
+    user: "root",
+    password: "Sequelites!8011", 
+    database: "testSystem"
+
+    
+}); 
